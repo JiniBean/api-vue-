9c837535e945fe3aed27e270e830af575f5a40d6
JiniBean_hi <JiniBean@DESKTOP-2BGN6PD>
2024-05-09 12:46:32 +0900

    jpa 배우는중

--- src/main/java/kr/co/rland/api/controller/MenuController.java
+++ src/main/java/kr/co/rland/api/controller/MenuController.java
@@ -3,10 +3,11 @@ package kr.co.rland.api.controller;
 import kr.co.rland.api.entity.Menu;
 import kr.co.rland.api.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,16 +20,41 @@ public class MenuController {
     private MenuRepository repository;
 
     @GetMapping
-    public List<Menu> list(){
+    public List<Menu> list(
+            @RequestParam(name = "p", required = false, defaultValue = "0") Integer page
+            , @RequestParam(name = "s", required = false, defaultValue = "6") Integer size
+            , @RequestParam(name = "e", required = false) String engName
+    ) {
+        //org.springframework.data.domain.Pageable 이니까 주의 같은 이름이 많음.
+        //Sort.by("reg_date").descending() 명령어를 통해서 data-set sort 가능, Sort.by 쓸 때에는 DB 컬럼명이 아닌, Entity의 속성명으로 작성해야 함.
+        Pageable pageable = PageRequest.of(page, size, Sort.by("engName").descending().and(Sort.by("regDate")));
 
-        List<Menu> list = repository.findAll();
+        //pageable이 반환하는 건, Page<Menu> 형태로 반환. 따라서 List<Menu>에 바로 담을 수 없음.
+        Page<Menu> menuPage = null;
+        if(engName !=null)
+            menuPage= repository.findByEngNameLike("%"+engName+"%", pageable);
+        else
+            menuPage = repository.findAll(pageable);
+
+        List<Menu> list = menuPage.getContent();
+
 
         return list;
     }
 
-    @GetMapping("{id}")
-    public Menu item(@PathVariable Long id){
-        Menu menu = new Menu();
-        return menu;
+
+    @GetMapping("/search/{engName}")
+    public List<Menu> findByEngName(@PathVariable String engName){
+        Pageable pageable = PageRequest.of(1,10, Sort.by("engName").descending().and(Sort.by("regDate")));
+        Page<Menu> menuPage = repository.findByEngNameLike("%"+engName+"%", pageable);
+        List<Menu> list = menuPage.getContent();
+
+        return list;
     }
+
+//    @GetMapping("{id}")
+//    public Menu item(@PathVariable Long id){
+//        Menu menu = new Menu();
+//        return menu;
+//    }
 }
--- src/main/java/kr/co/rland/api/repository/MenuRepository.java
+++ src/main/java/kr/co/rland/api/repository/MenuRepository.java
@@ -1,7 +1,13 @@
 package kr.co.rland.api.repository;
 
 import kr.co.rland.api.entity.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> { //구현해야할 걸 미리 정해뒀으니 물려받아라 <어느 엔티티, 엔티티식별자의 자료형>
+    // 아래와 같이 적은 것은 사용자 Custom 하는 방식임.
+
+    // Custom 해서 사용할 경우, Pageable 기능을 사용하려면 arguments에 Pageable을 추가 해야 함.
+    Page<Menu> findByEngNameLike(String engName, Pageable pageable);
 }
